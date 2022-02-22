921ce29a196e9bf825c9d5e3be5fdb4470aada03
Oleg Antonyák <oleg.antonyak@dxheroes.io>
2022-02-22 15:49:38 +0100

    feat: added index page

---
+++ index.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+require('ejs');
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 8080;
+const butter = require('buttercms')(process.env.BUTTER_CMS_TOKEN);
+
+const assetsPath = path.join(__dirname, './assets');
+const viewsPath = path.join(__dirname, "./views")
+
+app.use(express.static(assetsPath));
+
+// template engine setting
+app.set('view engine', 'ejs');
+app.set("views", viewsPath);
+
+app.get('/', (_, res) => {
+    butter.post.list({page_size:2,page: 1}).then(({data}) => {
+        res.render('index', {
+            posts: data.data,
+        })
+    });
+})
+
+app.get('/blog.html', (_,res) => {
+    res.render('blog')
+})
+
+app.listen(port)
